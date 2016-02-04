81f138b115a1aa8356e66ffbd3b6bea108c017fd
Phil Jauvin <philippejauvin@gmail.com>
2016-02-04 15:31:13 -0500

    Added better sorting, styling, infinite scroll

--- public/app.js
+++ public/app.js
@@ -1,18 +1,28 @@
-var app = angular.module("beats",["ng"]).config(function($sceProvider) {
+var app = angular.module("beats",["ng","infinite-scroll"]).config(function($sceProvider) {
   $sceProvider.enabled(false);
 });
 
 app.controller("SongController",["$scope","$http",function($scope,$http){
 
   $scope.sortBy = "data.score";
+  $scope.displayLimit = 8;
 
   $scope.sort = function(predicate){
+    $scope.displayLimit = 8;
     $scope.sortBy = predicate;
+  }
+
+  $scope.more = function(){
+    $scope.displayLimit += 8;
+  }
+
+  $scope.checkLoaded = function(){
+    return ($scope.displayLimit > 100);
   }
 
   $scope.songs = [];
 
-  $http.post("/api/songs/",{sub:"hiphopheads",flair:""}).success(function(res){
+  $http.post("/api/songs/",{sub:"listentothis",flair:"flair:Hip-hop",limit:50}).success(function(res){
 
     for(element of res){
       $scope.songs.push(element);
@@ -20,15 +30,12 @@ app.controller("SongController",["$scope","$http",function($scope,$http){
 
   });
 
-  $http.post("/api/songs/",{sub:"listentothis",flair:"flair:Hip-hop"}).success(function(res){
+  $http.post("/api/songs/",{sub:"hiphopheads",flair:"",limit:100-$scope.songs.length}).success(function(res){
 
     for(element of res){
       $scope.songs.push(element);
     }
 
-    console.log($scope.songs);
-
   });
-
 
 }]);
