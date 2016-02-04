bec2d8c4a83ec1f0835e9c48933694062dd148e4
Phil Jauvin <philippejauvin@gmail.com>
2016-02-03 21:37:14 -0500

    Added limited loading, sorting by new/top

--- public/app.js
+++ public/app.js
@@ -4,6 +4,12 @@ var app = angular.module("beats",["ng"]).config(function($sceProvider) {
 
 app.controller("SongController",["$scope","$http",function($scope,$http){
 
+  $scope.sortBy = "data.score";
+
+  $scope.sort = function(predicate){
+    $scope.sortBy = predicate;
+  }
+
   $scope.songs = [];
 
   $http.post("/api/songs/",{sub:"hiphopheads",flair:""}).success(function(res){
@@ -20,6 +26,9 @@ app.controller("SongController",["$scope","$http",function($scope,$http){
       $scope.songs.push(element);
     }
 
+    console.log($scope.songs);
+
   });
+
 
 }]);
